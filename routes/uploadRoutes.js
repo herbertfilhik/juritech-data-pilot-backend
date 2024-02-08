9ad7dd4e9b761ba9528ce1e9a0278437d614c3f5
herbertfilhik <herbertfilhik@gmail.com>
2024-02-08 20:17:00 -0300

    upload da planilha no mongo db atlas

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,8 +1,8 @@
-// routes/uploadRoutes.js
 const express = require('express');
 const multer = require('multer');
 const Documento = require('../models/Documento');
-const xlsx = require('xlsx'); // Se você estiver lidando com arquivos Excel
+const xlsx = require('xlsx');
+const moment = require('moment'); // Certifique-se de ter instalado a biblioteca moment
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
@@ -14,7 +14,6 @@ router.post('/upload', upload.single('file'), async (req, res) => {
   }
 
   try {
-    // Verifica se já existe um documento com o mesmo nome no banco de dados
     const existingDocumento = await Documento.findOne({ nome: file.originalname });
     if (existingDocumento) {
       return res.status(400).json({ message: 'Este arquivo já foi processado anteriormente.' });
@@ -23,18 +22,70 @@ router.post('/upload', upload.single('file'), async (req, res) => {
     const workbook = xlsx.readFile(file.path);
     const sheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[sheetName];
-    const data = xlsx.utils.sheet_to_json(worksheet);
+    const data = xlsx.utils.sheet_to_json(worksheet, { header: 1 }); // Use { header: 1 } para obter dados como array de arrays
 
-    const documento = new Documento({
-      nome: file.originalname,
-      dados: data,
-    });
+    // Remove o cabeçalho da planilha
+    data.shift(); // Remove a primeira linha (cabeçalho)
+    data.shift(); // Remove a segunda linha, que agora é a primeira após o primeiro shift()
 
-    const savedDocumento = await documento.save();
-    res.send({ message: 'Arquivo foi carregado e os dados foram salvos no banco de dados.', documentoId: savedDocumento._id });
+    // Função para converter data do Excel (OADate) para data JavaScript
+    function excelDateToJSDate(serial) {
+      const utc_days = Math.floor(serial - 25569);
+      const utc_value = utc_days * 86400;                                        
+      const date_info = new Date(utc_value * 1000);
+  
+      // Corrige a data considerando o fuso horário UTC para evitar deslocamentos indesejados
+      const correctedDate = new Date(date_info.getTime() + (date_info.getTimezoneOffset() * 60000));
+  
+      // Formata a data para o formato desejado (DD-MM-YYYY) usando moment.js
+      return moment(correctedDate).format('DD-MM-YYYY');
+    }
+  
+    // Mapeia os dados para o formato desejado
+    const mapeamentoDados = data.map((row) => ({
+      "Controle Target": row[0],
+      "Data de Solicitação": excelDateToJSDate(row[1]), // Já retorna 'DD-MM-YYYY'
+      "Data de Início":  excelDateToJSDate(row[2]),
+      "Solicitante": row[3],
+      "Grupo": row[4],
+      "Cliente": row[5],
+      "CNPJ": row[6],
+      "Município": row[7],
+      "UF": row[8],
+      "Deliberação": row[9],
+      "Ato Societário": row[10],
+      "Quantidade de impressão": row[11],
+      "Complexidade do Processo": row[12],
+      "Setor": row[13],
+      "Executor": row[14],
+      "Serviço": row[15],
+      "SLA": row[16],
+      "Cumprimento de SLA": row[17],
+      "Data do Protocolo": excelDateToJSDate(row[18]),
+      "Protocolo": row[19],
+      "Registro": row[20],
+      "Status": row[21],
+      "MÊS": row[22],
+      "Período Processual": row[23],
+      "Data de Finalização": excelDateToJSDate(row[24]),
+      "Codigo - Faturamento": row[25],
+      "STATUS": row[26],
+      "NF": row[27]
+    }));
+
+    // Salva cada documento mapeado no banco de dados
+    for (const documento of mapeamentoDados) {
+      const novoDocumento = new Documento({
+        nome: file.originalname,
+        dados: documento
+      });
+      await novoDocumento.save();
+    }
+
+    res.json({ message: 'Todos os registros foram processados e salvos com sucesso.' });
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Erro ao processar o arquivo');
+    console.error('Erro ao processar o arquivo:', error);
+    res.status(500).json({ message: 'Erro ao processar o arquivo', error: error.toString() });
   }
 });
 
