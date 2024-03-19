1bcb0ac314b03d6879f92d578f96d8265a7ab811
herbertfilhik <herbertfilhik@gmail.com>
2024-03-19 12:22:54 -0300

    include new screen for exclude and update register

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const cors = require('cors');
 const uploadRoutes = require('./routes/uploadRoutes'); // Certifique-se de que o caminho está correto
 const acompanhamentoServicoRoutes = require('./routes/acompanhamentoServicoRoutes'); // Importação da nova rota
+const incluireExcluirOperacao = require('./routes/incluireExcluirOperacao'); // Importação da nova rota
 const Documento = require('./models/Documento'); // O caminho deve ser ajustado para onde seu modelo Mongoose está localizado
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
@@ -16,32 +17,37 @@ app.use(bodyParser.json());
 
 // Usando o router com o prefixo /api
 app.use('/api', acompanhamentoServicoRoutes);
+app.use('/api', incluireExcluirOperacao);
 
 // Conexão com o MongoDB Atlas
 mongoose.connect(process.env.DB_URI)
   .then(() => console.log('Conexão com o MongoDB Atlas bem-sucedida'))
   .catch((err) => console.error('Erro ao conectar com o MongoDB Atlas', err));
 
-  const verifyToken = (req, res, next) => {
-    // Extrai o token do cabeçalho Authorization
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Divide a string pelo espaço e pega o segundo elemento (o token)
-  
-    if (!token) {
-      return res.status(403).send("Um token é necessário para a autenticação");
+const verifyToken = (req, res, next) => {
+  // Extrai o token do cabeçalho Authorization
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Divide a string pelo espaço e pega o segundo elemento (o token)
+
+  if (!token) {
+    return res.status(403).send("Um token é necessário para a autenticação");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+  } catch (err) {
+    // Verifica se o erro é especificamente devido a um token expirado
+    if (err.name === "TokenExpiredError") {
+      return res.status(401).send("Token expirado");
     }
-  
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded;
-    } catch (err) {
-      return res.status(401).send("Token inválido");
-    }
-  
-    return next();
-  };
-  
-  module.exports = verifyToken;
+    return res.status(401).send("Token inválido");
+  }
+
+  return next();
+};
+
+module.exports = verifyToken;
 
 const USERS = {
   admin: { password: 'senha123' }, // Em um cenário real, use hash de senha
@@ -52,7 +58,8 @@ app.post('/login', (req, res) => {
 
   if (USERS[username] && USERS[username].password === password) {
     // Se bem-sucedido, cria o token
-    const token = jwt.sign({ userId: USERS[username].id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    //const token = jwt.sign({ userId: USERS[username].id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    const token = jwt.sign({ userId: USERS[username].id }, process.env.JWT_SECRET, { expiresIn: '1m' });
     // Envia tanto a mensagem quanto o token na mesma resposta
     res.json({ message: 'Login bem-sucedido!', token });
   } else {
@@ -71,6 +78,19 @@ app.use('/', acompanhamentoServicoRoutes);
 
 // Rota para acompanhamento de serviço com filtro
 app.get('/acompanhamentoServico', verifyToken, async (req, res) => {
+  const { filtro } = req.query;
+  try {
+    // Substitua 'YourModel' pelo modelo Mongoose que você está usando para consultar os dados
+    const query = filtro ? { 'ControleTarget': new RegExp(filtro, 'i') } : {};
+    const resultados = await YourModel.find(query);
+    res.status(200).json(resultados);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao realizar a consulta no banco de dados', error });
+  }
+});
+
+// Rota para acompanhamento de serviço com filtro
+app.get('/incluireExcluirOperacao', verifyToken, async (req, res) => {
   const { filtro } = req.query;
   try {
     // Substitua 'YourModel' pelo modelo Mongoose que você está usando para consultar os dados
---
+++ routes/incluireExcluirOperacao.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+
+const Documento = require('../models/Documento'); // Ajuste o caminho conforme necessário
+
+router.get('/incluireExcluirOperacao', async (req, res) => {
+    const { filtro } = req.query;
+    try {
+      // Construa uma query que busca o filtro em múltiplos campos
+      let query = {};
+      if (filtro) {
+        query = {
+          $or: [
+            { 'dados.Controle Target': { $regex: filtro, $options: 'i' } },
+            { 'dados.Solicitante': { $regex: filtro, $options: 'i' } },
+            { 'dados.Cliente': { $regex: filtro, $options: 'i' } },
+            { 'dados.CNPJ': { $regex: filtro, $options: 'i' } },
+            { 'dados.Município': { $regex: filtro, $options: 'i' } },
+            { 'dados.UF': { $regex: filtro, $options: 'i' } },
+            { 'dados.Deliberação': { $regex: filtro, $options: 'i' } },
+            { 'dados.Ato Societário': { $regex: filtro, $options: 'i' } },
+            { 'dados.Quantidade de impressão': { $regex: filtro, $options: 'i' } },
+            { 'dados.Complexidade do Processo': { $regex: filtro, $options: 'i' } },
+            { 'dados.Setor': { $regex: filtro, $options: 'i' } },
+            { 'dados.Executor': { $regex: filtro, $options: 'i' } },
+            { 'dados.Serviço': { $regex: filtro, $options: 'i' } },
+            { 'dados.SLA': { $regex: filtro, $options: 'i' } },
+            { 'dados.Cumprimento de SLA': { $regex: filtro, $options: 'i' } },
+            { 'dados.Data do Protocolo': { $regex: filtro, $options: 'i' } },
+            { 'dados.Protocolo': { $regex: filtro, $options: 'i' } },
+            { 'dados.Registro': { $regex: filtro, $options: 'i' } },
+            { 'dados.Status': { $regex: filtro, $options: 'i' } },
+            { 'dados.MÊS': { $regex: filtro, $options: 'i' } },
+            { 'dados.Período Processual': { $regex: filtro, $options: 'i' } },
+            { 'dados.Data de Finalização': { $regex: filtro, $options: 'i' } },
+            { 'dados.STATUS': { $regex: filtro, $options: 'i' } },
+            { 'dados.NF': { $regex: filtro, $options: 'i' } },
+            // Adicione outras condições conforme necessário
+          ]
+        };
+      }
+      const documentos = await Documento.find(query);
+      res.status(200).json(documentos);
+    } catch (error) {
+      res.status(500).json({ message: 'Erro ao realizar a consulta no banco de dados', error });
+    }
+});
+
+module.exports = router;
